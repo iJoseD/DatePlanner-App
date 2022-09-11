156d73ec57807eca1e1e64a226aefe90393f231c
iJoseD <jcorrea@maando.com>
2022-09-11 18:43:37 -0500

    Update dashboard.php

--- layouts/dashboard.php
+++ layouts/dashboard.php
@@ -1,30 +1,11 @@
 <div class="container">
     <div class="row">
-        <div class="col s12">
-            <h1>¡BIENVENIDO!</h1>
-            <h2>¿Que quieres mirar hoy?</h2>
-        </div>
-    </div>
-
-    <!-- Accesos rapidos -->
-    <div class="row mt-5">
-        <div class="col-xl-3 col-sm-12 mb-3">
-            <a href="/historia-clinica" class="text-center text-white" style="text-decoration: none;">
-                <div class="card pt-5 pb-5 border border-light bg--AquaSpray">
-                    <div class="card-body">
-                        <h4>Historia Clínica</h4>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-xl-3 col-sm-12 mb-3">
-            <a href="/contabilidad/?day=&month=<?php echo date('F'); ?>&year=<?php echo date('Y'); ?>" class="text-center text-white" style="text-decoration: none;">
-                <div class="card pt-5 pb-5 border border-light bg--AquaSpray">
-                    <div class="card-body">
-                        <h4>Contabilidad</h4>
-                    </div>
-                </div>
-            </a>
+        <div class="col s12 m5">
+            <div class="card-panel teal">
+                <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+                    I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                </span>
+            </div>
         </div>
     </div>
 </div>
