e8e1fc9f507e4d717d32bcd353e2607c91f94951
iJoseD <jcorrea@maando.com>
2022-09-11 18:08:20 -0500

    Commitn Inicial

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="noindex">
+        <meta name="googlebot" content="noindex">
+
+        <!-- Favicon -->
+        <link rel="icon" type="image/x-icon" href="../dist/img/favicon.png">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="../dist/style.css">
+
+        <title>Backoffice</title>
+    </head>
+    
+    <body>
+        <!-- Navbar -->
+        <?php require_once('layouts/navbar.php'); ?>
+
+        <!-- Dashboard -->
+        <?php require_once('layouts/dashboard.php'); ?>
+
+        <!-- Bootstrap Bundle with Popper -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
