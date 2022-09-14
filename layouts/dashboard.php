8caf1cd4772fb591d0321e1d851db0ffd0414c5a
iJoseD <jcorrea@maando.com>
2022-09-14 18:28:27 -0500

    Update dashboard.php

--- layouts/dashboard.php
+++ layouts/dashboard.php
@@ -1,12 +1,39 @@
 <div class="container">
-    <div class="row">
+    <div class="row mt-5">
+        <!-- Reservas -->
         <div class="col-4">
-            <div class="card">
-                <div class="card-body">
-                    <img src="http://demo.dateplannerus.com/administrador/assets/img/calendar.svg" class="card-img-top">
-                    <p class="card-text">Reservas</p>
+            <a href="/administrador/nomina/">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="http://demo.dateplannerus.com/administrador/assets/img/calendar.svg">
+                        <p class="card-text">Reservas</p>
+                    </div>
                 </div>
-            </div>
+            </a>
+        </div>
+
+        <!-- Contabilidad -->
+        <div class="col-4">
+            <a href="/administrador/nomina/">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="http://demo.dateplannerus.com/administrador/assets/img/accounting.svg">
+                        <p class="card-text">Contabilidad</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <!-- Nomina -->
+        <div class="col-4">
+            <a href="/administrador/nomina/">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="http://demo.dateplannerus.com/administrador/assets/img/salary.svg">
+                        <p class="card-text">Nomina</p>
+                    </div>
+                </div>
+            </a>
         </div>
     </div>
 </div>
