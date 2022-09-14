44bd901b051daadf5464d5f370e997220a40cd70
iJoseD <jcorrea@maando.com>
2022-09-14 16:05:02 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 
         <!-- Bootstrap 5.2.1 -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+        <!-- Title -->
+        <title>DatePlanner App</title>
     </head>
     
     <body>
