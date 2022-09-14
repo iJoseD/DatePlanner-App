e7e3f00a9e550a2576713dd5d97646453eee51c2
iJoseD <jcorrea@maando.com>
2022-09-14 15:51:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,13 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
     <head>
-        <!--Import Google Icon Font-->
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!--Import materialize.css-->
-        <link type="text/css" rel="stylesheet" href="../dist/css/materialize.min.css"  media="screen,projection"/>
-
-        <!--Let browser know website is optimized for mobile-->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <meta name="robots" content="noindex">
-        <meta name="googlebot" content="noindex">
-
-        <!-- Favicon -->
-        <link rel="icon" type="image/x-icon" href="../dist/img/favicon.png">
-
-        <!-- Title -->
-        <title>DatePlanner App</title>
+        <!-- Bootstrap 5.2.1 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     </head>
-
+    
     <body>
         <!-- Navbar -->
         <?php require_once('layouts/navbar.php'); ?>
@@ -29,8 +18,8 @@
         <!-- jQuery 3.6.1 -->
         <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 
-        <!--JavaScript at end of body for optimized loading-->
-        <script type="text/javascript" src="../dist/js/materialize.min.js"></script>
-        <script type="text/javascript" src="../dist/js/main.min.js"></script>
+        <!-- Bootstrap 5.2.1 -->
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
     </body>
 </html>
