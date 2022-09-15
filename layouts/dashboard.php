ee2715a02bf2ba6fb0a0e6aedbb93aed60088514
iJoseD <jcorrea@maando.com>
2022-09-15 17:45:13 -0500

    Update dashboard.php

--- layouts/dashboard.php
+++ layouts/dashboard.php
@@ -3,28 +3,28 @@
         <!-- Reservas -->
         <div class="col-3">
             <a href="/administrador/reservas/" style="text-decoration: none;">
-                <img src="../dist/img/Widget-1.svg" style="width: 100%;">
+                <img src="./dist/img/Widget-1.svg" style="width: 100%;">
             </a>
         </div>
 
         <!-- Contabilidad -->
         <div class="col-3">
             <a href="/administrador/contabilidad/?day=&month=<?php echo date('F'); ?>&year=<?php echo date('Y'); ?>" style="text-decoration: none;">
-                <img src="../dist/img/Widget-2.svg" style="width: 100%;">
+                <img src="./dist/img/Widget-2.svg" style="width: 100%;">
             </a>
         </div>
 
         <!-- Nomina -->
         <div class="col-3">
             <a href="/administrador/nomina/" style="text-decoration: none;">
-                <img src="../dist/img/Widget-3.svg" style="width: 100%;">
+                <img src="./dist/img/Widget-3.svg" style="width: 100%;">
             </a>
         </div>
 
         <!-- Inventario -->
         <div class="col-3">
             <a href="/administrador/inventario/" style="text-decoration: none;">
-                <img src="../dist/img/Widget-4.svg" style="width: 100%;">
+                <img src="./dist/img/Widget-4.svg" style="width: 100%;">
             </a>
         </div>
     </div>
@@ -33,14 +33,14 @@
         <!-- Facturación -->
         <div class="col-3">
             <a href="/administrador/pos/?idBooking=" style="text-decoration: none;">
-                <img src="../dist/img/Widget-5.svg" style="width: 100%;">
+                <img src="./dist/img/Widget-5.svg" style="width: 100%;">
             </a>
         </div>
 
         <!-- Empleados -->
         <div class="col-3">
             <a href="/administrador/empleados/" style="text-decoration: none;">
-                <img src="../dist/img/Widget-6.svg" style="width: 100%;">
+                <img src="./dist/img/Widget-6.svg" style="width: 100%;">
             </a>
         </div>
     </div>
