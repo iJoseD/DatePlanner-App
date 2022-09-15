9491f19a0cc1f447e39d3c064bf2ec862e1a025e
iJoseD <jcorrea@maando.com>
2022-09-14 19:00:00 -0500

    Update dashboard.php

--- layouts/dashboard.php
+++ layouts/dashboard.php
@@ -3,59 +3,21 @@
         <!-- Reservas -->
         <div class="col-4">
             <a href="/administrador/reservas/" style="text-decoration: none;">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <img src="http://demo.dateplannerus.com/administrador/assets/img/calendar.svg" style="width: 20%;">
-                        <p class="card-text">Reservas</p>
-                    </div>
-                </div>
+                <img src="../dist/img/Widget-1.svg" style="width: 100%;">
             </a>
         </div>
 
         <!-- Contabilidad -->
         <div class="col-4">
             <a href="/administrador/contabilidad/?day=&month=<?php echo date('F'); ?>&year=<?php echo date('Y'); ?>" style="text-decoration: none;">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <img src="http://demo.dateplannerus.com/administrador/assets/img/accounting.svg" style="width: 20%;">
-                        <p class="card-text">Contabilidad</p>
-                    </div>
-                </div>
+                <img src="../dist/img/Widget-2.svg" style="width: 100%;">
             </a>
         </div>
 
         <!-- Nomina -->
         <div class="col-4">
             <a href="/administrador/nomina/" style="text-decoration: none;">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <img src="http://demo.dateplannerus.com/administrador/assets/img/salary.svg" style="width: 20%;">
-                        <p class="card-text">Nomina</p>
-                    </div>
-                </div>
-            </a>
-        </div>
-    </div>
-
-    <div class="row">
-        <!-- Reservas -->
-        <div class="col-4">
-            <a href="/administrador/reservas/" style="text-decoration: none;">
-                <img src="../dist/img/Widget-1.svg">
-            </a>
-        </div>
-
-        <!-- Contabilidad -->
-        <div class="col-4">
-            <a href="/administrador/reservas/" style="text-decoration: none;">
-                <img src="../dist/img/Widget-2.svg">
-            </a>
-        </div>
-
-        <!-- Nomina -->
-        <div class="col-4">
-            <a href="/administrador/reservas/" style="text-decoration: none;">
-                <img src="../dist/img/Widget-3.svg">
+                <img src="../dist/img/Widget-3.svg" style="width: 100%;">
             </a>
         </div>
     </div>
