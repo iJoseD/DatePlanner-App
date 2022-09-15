0668ffdd827eecb5839421bc0ed01c9bbda58c1a
iJoseD <jcorrea@maando.com>
2022-09-15 14:57:51 -0500

    Update dashboard.php

--- layouts/dashboard.php
+++ layouts/dashboard.php
@@ -1,21 +1,21 @@
 <div class="container">
     <div class="row mt-5">
         <!-- Reservas -->
-        <div class="col-4">
+        <div class="col-3">
             <a href="/administrador/reservas/" style="text-decoration: none;">
                 <img src="../dist/img/Widget-1.svg" style="width: 100%;">
             </a>
         </div>
 
         <!-- Contabilidad -->
-        <div class="col-4">
+        <div class="col-3">
             <a href="/administrador/contabilidad/?day=&month=<?php echo date('F'); ?>&year=<?php echo date('Y'); ?>" style="text-decoration: none;">
                 <img src="../dist/img/Widget-2.svg" style="width: 100%;">
             </a>
         </div>
 
         <!-- Nomina -->
-        <div class="col-4">
+        <div class="col-3">
             <a href="/administrador/nomina/" style="text-decoration: none;">
                 <img src="../dist/img/Widget-3.svg" style="width: 100%;">
             </a>
@@ -24,21 +24,21 @@
 
     <div class="row">
         <!-- Inventario -->
-        <div class="col-4">
+        <div class="col-3">
             <a href="/administrador/inventario/" style="text-decoration: none;">
                 <img src="../dist/img/Widget-4.svg" style="width: 100%;">
             </a>
         </div>
 
         <!-- Facturación -->
-        <div class="col-4">
+        <div class="col-3">
             <a href="/administrador/pos/?idBooking=" style="text-decoration: none;">
                 <img src="../dist/img/Widget-5.svg" style="width: 100%;">
             </a>
         </div>
 
         <!-- Empleados -->
-        <div class="col-4">
+        <div class="col-3">
             <a href="/administrador/empleados/" style="text-decoration: none;">
                 <img src="../dist/img/Widget-6.svg" style="width: 100%;">
             </a>
